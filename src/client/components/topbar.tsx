69c7373fb589747e9e4fb14d188f48497463abb7
Javier Lurgo <jlurgo@gmail.com>
2024-10-11 18:20:56 -0300

    theme toggle

--- src/client/components/topbar.tsx
+++ src/client/components/topbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { ModeToggle } from "./ui/mode-toggle";
 
 export function TopBar() {
-    return <div className="col-span-2 p-2 shadow flex flex-row flex-nowrap">
+    return <div className="col-span-2 p-2 shadow flex flex-row flex-nowrap gap-1">
         <img src="/src/client/assets/logo.svg" className="h-9 flex-none"/>
         <div className="grow h-9 text-3xl pl-2">Transact</div>
+        <ModeToggle />
         <Avatar className="flex-none h-9">
             <AvatarImage src="/src/client/assets/avatar.jpg" alt="User avatar"/>
             <AvatarFallback>U</AvatarFallback>
