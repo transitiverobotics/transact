71754207010541ab2e060341a51ce7aa0c904ae9
Christian Fritz <christian.fritz@gmail.com>
2024-10-11 15:34:00 -0700

    Make port customizable

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,11 +2,13 @@ import express from "express";
 import ViteExpress from "vite-express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+
+ViteExpress.listen(app, port, () =>
+  console.log(`Server is listening on port ${port}...`),
 );
