695e3214415d0a815a66c63ff21048c614c4d0bd
Javier Lurgo <jlurgo@gmail.com>
2024-10-11 18:40:47 -0300

    devices shown in a table

--- src/client/sections/devicessection.tsx
+++ src/client/sections/devicessection.tsx
@@ -1,32 +1,40 @@
 import React from "react";
 import { my_fake_devices } from "../models/device";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "../components/ui/card"
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "../components/ui/table";
 
 export function DevicesSection() {
     return (
-        <div className="flex flex-wrap gap-4">
-            {my_fake_devices.map((device, i) => (
-            <Card key={`${device}-${i}`} className="flex flex-col justify-between min-w-[200px] max-w-[300px]">
-                <CardHeader>
-                    <CardTitle>{device.name}</CardTitle>
-                </CardHeader>
-                <CardContent className="">
-                    <CardDescription className="">
-                        <img src={device.type.icon_svg} alt={device.name} className="max-h-full max-w-full"/>
-                    </CardDescription>
-                </CardContent>
-                <CardFooter className="">
-                <p>{device.description}</p>
-                </CardFooter>
-            </Card>)
-            )}
-        </div>
+        <Table>
+        <TableCaption>Your devices</TableCaption>
+        <TableHeader>
+            <TableRow>
+            <TableHead className="w-[200px]">Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead className="text-right">Battery</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            {my_fake_devices.map((device) => (
+            <TableRow key={device.name}>
+                <TableCell>
+                <div className="flex items-center gap-2">
+                    <img src={device.type.icon_svg} className="h-6" alt={device.type.name} />
+                    {device.name}
+                </div>
+                </TableCell>
+                <TableCell>{device.description}</TableCell>
+                <TableCell className="text-right">{device.battery_level}%</TableCell>
+            </TableRow>
+            ))}
+        </TableBody>
+        </Table>
     );
 }
