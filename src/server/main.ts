b14ced49e435d5afbe3bfe0fefb1a8742b90580f
Christian Fritz <christian.fritz@gmail.com>
2024-10-12 20:39:17 -0700

    Code for getting JWT from front-end
    Some code I had in https://github.com/transitiverobotics/examples for getting a JWT from the front-end (we still need to add authentication). Also shows how to get env vars to the front-end in Vite.

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,28 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config({path: './.env'});
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+const app = express();
+app.use(express.json());
+
+app.get('/hello', (_, res) => {
+  res.send('Hello Vite + React + TypeScript!');
+});
+
+
+app.post('/getJWT', (req, res) => {
+  console.log('getJWT', req.body, req.user);
+  const token = jwt.sign({
+      ...req.body,
+      id: process.env.VITE_TRANSITIVE_USER, // Transitive portal user id
+      userId: req.user,  // user name on dashboard
+      validity: 86400,   // number of seconds
+    }, process.env.JWT_SECRET);
+  res.json({token});
 });
 
 
