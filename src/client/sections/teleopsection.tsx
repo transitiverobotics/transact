9ec4963df5926be4870b2b9927087eb3e532caec
Christian Fritz <christian.fritz@gmail.com>
2024-10-14 21:07:33 -0700

    minor changes

--- src/client/sections/teleopsection.tsx
+++ src/client/sections/teleopsection.tsx
@@ -7,20 +7,20 @@ export function TeleopSection() {
 
   return (
     <div className="">
-        <DeviceSelector onChange={setDevice}/>
-        {device}
-        <JWTCapability 
-            device={device}
-            capability={"@transitive-robotics/remote-teleop"}
-            control_rosVersion="1"
-            control_topic="/joy"
-            control_type="sensor_msgs/Joy"
-            count="1"
-            quantizer="25"
-            timeout="1800"
-            type="videotestsrc"
-        >
-        </JWTCapability>
+      <DeviceSelector onChange={setDevice}/>
+      {device}
+      {device && <JWTCapability
+          device={device}
+          capability={"@transitive-robotics/remote-teleop"}
+          control_rosVersion="1"
+          control_topic="/joy"
+          control_type="sensor_msgs/Joy"
+          count="1"
+          quantizer="25"
+          timeout="1800"
+          type="videotestsrc"
+          />
+      }
     </div>
   );
 }
