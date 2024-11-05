d5b170dfe0d6bc6a485377ef915d6d0cae3877d0
Javier Lurgo <jlurgo@gmail.com>
2024-11-05 15:25:20 -0300

    Using /battery_status topic

--- src/client/components/battery-indicator.tsx
+++ src/client/components/battery-indicator.tsx
@@ -14,17 +14,17 @@ export const BatteryIndicator = ({device}) => {
 
   useEffect(() => {
     const _api = capabilityContext.getAPI(device);
-    log.debug('Subscribing to topic: /status/battery for device', device);
-    _api?.subscribe(1, '/status/battery');
+    log.debug('Subscribing to topic: /battery_status for device', device);
+    _api?.subscribe(1, '/battery_status');
     // unsubscribe when React component unmounts
     setApi(_api);
     return () => {
-      log.debug('Unsubscribing from topic: /status/battery for device', device);
-      _api?.unsubscribe?.(1, '/status/battery');
+      log.debug('Unsubscribing from topic: /battery_status for device', device);
+      _api?.unsubscribe?.(1, '/battery_status');
     }
   }, [capabilityContext?.ready]);
 
-  const batteryStatus = api?.deviceData?.ros?.[1].messages?.status?.battery;
+  const batteryStatus = api?.deviceData?.ros?.[1].messages?.battery_status;
 
   return <div>
     {!batteryStatus ? <BatteryWarning /> :
